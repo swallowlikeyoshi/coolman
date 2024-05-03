f30766fd96bae7d3ba277417e52d16ba42687f57
Dohyeon Kim <xingchen8837@gmail.com>
2024-05-03 13:23:55 +0900

    gpt api 연동 제작

--- answer/gpt.py
+++ answer/gpt.py
@@ -1,8 +1,29 @@
+import requests
+import json
 
+URL = 'http://ollamaunyang.kro.kr/api/generate'
 
 def ask(problem):
-
-    # 여기에 GPT 처리하는 코드 추가
-    answer = problem
-
+    prompt = {
+        'model': 'llama3',
+        'prompt': "내가 한국어로 질문을 줄게, 앞으로는 한국어로만 말하고 한국어로 대답하면 돼. 이제 다음 질문에 답해줘: " + problem,
+    }
+    response = _get_post_response(URL, prompt)
+    answer = _process_text(response.text)
     return answer
+
+def _get_post_response(URL, params):
+    return requests.post(URL, json=params)
+
+def _process_text(response_text):
+    # print(response_text)
+    responses = response_text.strip().split('\n')
+    answer = ''
+    for token in responses:
+        answer += json.loads(token)['response']
+    return answer
+
+if __name__ == "__main__":
+    while True:
+        prompt = input(">>> ")
+        print(ask(prompt))
